0c1e5fd307da3dfa112577af1cfa7b88fe09e514
jordan7510 <cgupta7510@gmail.com>
2023-12-08 16:02:53 +0530

    updated'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // const publicIp = "16.171.62.122";
+// const publicIp = "16.171.62.122";
+// const publicIp = "16.171.62.122";
 
 // https://vitejs.dev/config/
 export default defineConfig({
